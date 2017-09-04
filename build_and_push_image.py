fa0fac47dbd75e678587a3b085ebe3ee29984a8a
Bart Spaans <bart.spaans@gmail.com>
2017-09-04 12:00:13 +0100

    Add 'docker_import_image' option which skips the build and makes an external image available

--- build_and_push_image.py
+++ build_and_push_image.py
@@ -7,32 +7,27 @@ import subprocess
 sys.path = ["deps/_/stdlib"] + sys.path
 import escape
 
-outputs_path = sys.argv[1]
-
 env = os.environ
-docker_image = env['INPUT_docker_image']
-docker_image_version = env['INPUT_docker_image_version']
-docker_file = env['INPUT_docker_file']
-docker_repo = env['INPUT_docker_repository']
-docker_username = env['INPUT_docker_username']
-docker_password = env['INPUT_docker_password']
-docker_email = env['INPUT_docker_email']
-docker_server = env['INPUT_docker_server']
+docker_image = escape.get_string_input('docker_image')
+docker_image_version = escape.get_string_input('docker_image_version')
+docker_file = escape.get_string_input('docker_file')
+docker_repo = escape.get_string_input('docker_repository')
+docker_username = escape.get_string_input('docker_username')
+docker_password = escape.get_string_input('docker_password')
+docker_email = escape.get_string_input('docker_email')
+docker_server = escape.get_string_input('docker_server')
+import_image = escape.get_bool_input('docker_import_image')
 should_push = docker_repo != ''
 should_login = docker_username != ''
 docker_cmd = json.loads(env['INPUT_docker_cmd'])
 
-if not os.path.exists(docker_file):
-    print "Referenced docker file '%s' does not exist" % docker_file
-    sys.exit(1)
-
-base_image_name = docker_image
-if should_push:
-    if not docker_repo.endswith("/"):
-        docker_repo = docker_repo + "/"
-    base_image_name = docker_repo + base_image_name
-
-versioned_base_image = base_image_name + ":" + docker_image_version
+def get_base_image(docker_repo, docker_image, should_push):
+    base_image_name = docker_image
+    if should_push:
+        if not docker_repo.endswith("/"):
+            docker_repo = docker_repo + "/"
+        base_image_name = docker_repo + base_image_name
+    return base_image_name
 
 
 def run_docker(cmd):
@@ -45,7 +40,8 @@ def run_docker(cmd):
         sys.exit(1)
     return output
 
-if should_login:
+
+def login(docker_cmd, docker_username, docker_password, docker_email, docker_server):
     cmd = docker_cmd + ["login", "--username", docker_username, "--password", docker_password]
     if docker_email != '':
         cmd += ["--email", docker_email]
@@ -53,25 +49,41 @@ if should_login:
         cmd += [docker_server]
     run_docker(cmd)
 
-print "Building Docker image '%s'" % versioned_base_image
 
-cmd = docker_cmd + ["build", "-f", docker_file, "-t", versioned_base_image, "."]
-run_docker(cmd)
+def build(docker_file, versioned_base_image, docker_cmd, base_image_name):
+    if not os.path.exists(docker_file):
+        print "Referenced docker file '%s' does not exist" % docker_file
+        sys.exit(1)
 
-print "Tagging '%s:latest'" % base_image_name
-cmd = docker_cmd + ["tag", versioned_base_image, base_image_name + ":latest"]
-try:
-    subprocess.check_output(cmd)
-except:
-    # Fallback for Docker <1.10
-    cmd = docker_cmd + ["tag", "-f", versioned_base_image, base_image_name + ":latest"]
+    print "Building Docker image '%s'" % versioned_base_image
+
+    cmd = docker_cmd + ["build", "-f", docker_file, "-t", versioned_base_image, "."]
     run_docker(cmd)
 
-if should_push:
-    run_docker(docker_cmd + ["push", versioned_base_image])
-    run_docker(docker_cmd + ["push", base_image_name + ":latest"])
+    print "Tagging '%s:latest'" % base_image_name
+    cmd = docker_cmd + ["tag", versioned_base_image, base_image_name + ":latest"]
+    try:
+        subprocess.check_output(cmd)
+    except:
+        # Fallback for Docker <1.10
+        cmd = docker_cmd + ["tag", "-f", versioned_base_image, base_image_name + ":latest"]
+        run_docker(cmd)
 
-outputs = json.load(open(outputs_path))
-outputs["image"] = versioned_base_image
-with open(outputs_path, "w") as f:
-    json.dump(outputs, f)
+
+base_image_name = get_base_image(docker_repo, docker_image, should_push)
+versioned_base_image = base_image_name + ":" + docker_image_version
+
+if not import_image:
+    if should_login:
+        login(docker_cmd, docker_username, docker_password, docker_email, docker_server)
+    build(docker_file, versioned_base_image, docker_cmd, base_image_name)
+
+    if should_push:
+        run_docker(docker_cmd + ["push", versioned_base_image])
+        run_docker(docker_cmd + ["push", base_image_name + ":latest"])
+else:
+    print "Skipping build, using image:", versioned_base_image
+
+outputs = escape.get_outputs()
+outputs.set_output("image", versioned_base_image)
+outputs.save()
